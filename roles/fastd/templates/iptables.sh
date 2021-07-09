d22abce7217c01de66b5e0ef25ea19a82bddf349
Benjamin Brahmer <info@b-brahmer.de>
2021-07-09 20:15:57 +0200

    add firewall rules to prevent network scan

--- roles/fastd/templates/iptables.sh
+++ roles/fastd/templates/iptables.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
 /sbin/ip route add 10.144.0.0/16 dev br-ffsh src {{ ffsh_ipv4_address }} table 42
+
+# Reject forwarded outgoing packets on external WAN interface to private IP addresses
+/sbin/iptables -I FORWARD -d 10.0.0.0/8 -o {{ external_interface }} -j REJECT
+/sbin/iptables -I FORWARD -d 172.16.0.0/12 -o {{ external_interface }} -j REJECT
+/sbin/iptables -I FORWARD -d 192.168.0.0/16 -o {{ external_interface }} -j REJECT
+/sbin/iptables -I FORWARD -d 100.64.0.0/10 -o {{ external_interface }} -j REJECT
+/sbin/iptables -I FORWARD -d 169.254.0.0/16 -o {{ external_interface }} -j REJECT
+
 /sbin/iptables -t nat -I POSTROUTING -s 0/0 -d 0/0 -j MASQUERADE
 # IGMP/MLD segmentation
 echo 2 > /sys/class/net/bat0/brport/multicast_router
