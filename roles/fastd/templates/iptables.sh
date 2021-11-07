e0e11728493426a0ff81572d19b74717984fe914
Benjamin Brahmer <info@b-brahmer.de>
2021-11-07 15:39:06 +0100

    last bits to make wireguard work

--- roles/fastd/templates/iptables.sh
+++ roles/fastd/templates/iptables.sh
@@ -9,6 +9,10 @@
 /sbin/iptables -I FORWARD -d 100.64.0.0/10 -o {{ external_interface }} -j REJECT
 /sbin/iptables -I FORWARD -d 169.254.0.0/16 -o {{ external_interface }} -j REJECT
 
+# this is not relevant when the gateway uses a direct exit but important when table 42 has a special interface as exit
+/sbin/iptables -t mangle -I PREROUTING -s 10.144.0.0/16 -j MARK --set-mark 0x1
+
+# nat, exit ip will be based on the route
 /sbin/iptables -t nat -I POSTROUTING -s 10.144.0.0/16 -d 0/0 -j MASQUERADE
 # IGMP/MLD segmentation
 echo 2 > /sys/class/net/bat0/brport/multicast_router
